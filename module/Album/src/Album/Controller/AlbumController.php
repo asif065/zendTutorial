c30c1e9c19e6f1ddc3e134d94fc1d19a599333aa
Khondoker Asif Akter <asif@asif-macbook.local>
2015-03-03 11:04:03 +0600

    initial commit

---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController {
+
+    protected $albumTable;
+
+    public function indexAction() {
+        // grab the paginator from the AlbumTable
+        $paginator = $this->getAlbumTable()->fetchAll(true);
+        // set the current page to what has been passed in query string, or to 1 if none set
+        $paginator->setCurrentPageNumber((int) $this->params()->fromQuery('page', 1));
+        // set the number of items per page to 10
+        $paginator->setItemCountPerPage(10);
+
+        return new ViewModel(array(
+            'paginator' => $paginator
+        ));
+    }
+
+    public function addAction() {
+        
+    }
+
+    public function editAction() {
+        
+    }
+
+    public function deleteAction() {
+        
+    }
+
+    public function getAlbumTable() {
+        if (!$this->albumTable) {
+            $sm = $this->getServiceLocator();
+            $this->albumTable = $sm->get('Album\Model\AlbumTable');
+        }
+        return $this->albumTable;
+    }
+
+}
